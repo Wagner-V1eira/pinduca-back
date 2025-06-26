0b2f6d159caa2260fa459a376af9ea8f04ade0cc
Wagner Rosa <wagnervieira.rosa@hotmail.com>
2025-06-26 10:01:23 -0300

    Ajustes no deploy

--- index.ts
+++ index.ts
@@ -1,24 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv'; 
 import routesGibi from './routes/gibi';
 import routesUsuario from './routes/usuario';
 import routesLogin from './routes/login'; 
 import authRouter from './routes/auth';
 import routesReview from './routes/review';
 
-dotenv.config();
-
 const app = express();
+const port = 3001
 
 app.use(express.json());
 
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000'; 
 console.log(`[CORS] Configurando para permitir origem: ${frontendUrl}`);
-app.use(cors({
-    origin: frontendUrl, 
-    credentials: true 
-}));
+app.use(cors())
 
 app.use("/api/gibi", routesGibi);
 app.use("/api/usuario", routesUsuario);
@@ -30,11 +25,8 @@ app.get('/api', (req, res) => {
   res.status(200).json({ message: 'API Pinduca Reviews está operacional!' });
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3001; 
-  app.listen(PORT, () => {
-    console.log(`[API Local Condicional] Servidor rodando na porta: ${PORT}`);
-  });
-}
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+})
 
 export default app;
